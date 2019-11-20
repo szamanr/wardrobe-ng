b452725ac15ac7653733b815f8923aabd20437c9
rrrafalsz <rrrafalsz@gmail.com>
2019-11-20 15:10:01 +0100

    implement calculating thermometer & use local images

--- src/app/cloth-pick/cloth-pick.component.ts
+++ src/app/cloth-pick/cloth-pick.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Cloth} from '../cloth';
-import {getSourceFile} from 'tslint';
-import {CLOTHES} from '../../assets/clothes';
+import {CLOTHES} from '../../assets/mocks/clothes';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-cloth-pick',
@@ -9,15 +9,86 @@ import {CLOTHES} from '../../assets/clothes';
   styleUrls: ['./cloth-pick.component.scss']
 })
 export class ClothPickComponent implements OnInit {
+  /**
+   * list of available clothes
+   */
   private clothes: Cloth[];
+
+  /**
+   * clothes currently selected for wearing
+   */
+  private clothSet: boolean[] = [];
+
+  /**
+   * the value indicating how cold / warm we'll feel if wearing the currently selected clothes in the current weather
+   */
+  thermometer: number;
+
+  /**
+   * current climate info: temperature, wind, precipitation
+   */
+  private climate: any;
 
   constructor() { }
 
   ngOnInit() {
-    this.getClothes();
+    this.clothes = this.getClothes();
+    this.climate = this.getCurrentClimate();
+
+    this.updateThermometer();
   }
 
   private getClothes() {
-    this.clothes = CLOTHES;
+    return CLOTHES;
+  }
+
+  /**
+   * adds / removes cloth from current set
+   * @param cloth the selected cloth item
+   */
+  toggle(cloth: Cloth) {
+    this.clothSet[cloth.id] = (typeof this.clothSet[cloth.id] !== 'undefined') ? !this.clothSet[cloth.id] : true;
+
+    console.log(this.clothSet[cloth.id]);
+
+    if (!environment.production) {
+      console.log(`current cloth set: ${this.clothSet.toString()}`);
+    }
+
+    this.updateThermometer();
+  }
+
+  private updateThermometer() {
+    const selectedClothes = this.clothes.filter((cloth) => {
+      return this.clothSet[cloth.id];
+    });
+
+    // compute the sum of temperatures of all clothes in the current set
+    const currentSetTemperature = selectedClothes.map((item) => {
+      return item.temperature;
+    }).reduce((a, b) => a + b, 0);
+
+    // compare that with the current temperature to get the thermometer value
+    const feltTemperature = this.computeFeltTemperature();
+    this.thermometer = currentSetTemperature - feltTemperature;
+  }
+
+  /**
+   * computes temperature felt based on temperature, wind and precipitation
+   */
+  private computeFeltTemperature() {
+    return this.climate.temperature - Math.round(this.climate.wind / 10) - 5 * this.climate.rain - 5 * this.climate.snow;
+  }
+
+  /**
+   * gets current climate condition from weather service
+   */
+  private getCurrentClimate() {
+    return {
+      temperature: 10,
+      wind: 0,
+      rain: false,
+      snow: false
+    };
   }
 }
---
+++ src/assets/mocks/clothes.ts
@@ -0,0 +1,10 @@
+import {Cloth} from '../../app/cloth';
+
+export const CLOTHES: Cloth[] = [
+  new Cloth(4, 'body', 'assets/images/cloth01.jpg'),
+  new Cloth(2, 'body', 'assets/images/cloth02.jpg'),
+  new Cloth(10, 'body', 'assets/images/cloth03.jpg'),
+  new Cloth(15, 'head', 'assets/images/cloth04.jpg'),
+  new Cloth(10, 'feet', 'assets/images/cloth05.jpg'),
+  new Cloth(6, 'body', 'assets/images/cloth06.jpg')
+];
